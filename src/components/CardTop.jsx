58b6c0c34511d5183e70ac2264357955b5d1f522
Sheetal Naik <sheetalnaik848@gmail.com>
2025-03-29 18:12:38 +0530

    Update CardTop.jsx

--- src/components/CardTop.jsx
+++ src/components/CardTop.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import '../css/CardTop.css'
 
 const CardTop = ({ number, image, upSubscribers, social, followers, color, colorUpDown, downup }) => {
+
+  const corBorder = { borderColor: color }
+  const UpDown = { color: colorUpDown }
+
   return (
     <>
-    <section className="card-top">
+    <section className="card-top" style={corBorder}>
         <div className="topo">
             <img src={image} alt={image} />
             <h4>{social}</h4>
@@ -12,7 +16,7 @@ const CardTop = ({ number, image, upSubscribers, social, followers, color, color
         <div className="rodape-top">
             <h4>{number}</h4>
             <h5>{followers}</h5>
-            <h6>{downup}{upSubscribers}</h6>
+            <h6 style={UpDown}>{downup}{upSubscribers}</h6>
         </div>
     </section>
     </>
